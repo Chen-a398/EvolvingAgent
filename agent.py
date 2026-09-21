d9a681e8d0049c04554e8b347de765d37acbd791
Chen-a398 <a13725364618@163.com>
2026-09-21 13:20:52 +0800

    agent

--- agent.py
+++ agent.py
@@ -8,10 +8,10 @@ from typing import Optional
 
 import tiktoken
 
-from .llm import LLMClient
+from .LLM import LLMClient
 from .logger import AgentLogger
 from .schema import Message
-from .tools.base import Tool, ToolResult
+from .Tool.base import Tool, ToolResult
 from .utils import calculate_display_width
 
 
